6dcd8111c50f3cbf02379c20d7297fa63c8c00a7
siqingxiao <siqingxiao@fnic.cn>
2025-05-06 08:55:52 +0800

    fix: text change

--- demos/views/Simple.vue
+++ demos/views/Simple.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="el-tiptap-editor__wrapper">
-    <el-tiptap :extensions="extensions" v-model:content="content" output="json" placeholder="Write something ..."
-      @onSelectionUpdate="onSelectionUpdate" />
+    <el-tiptap ref="editor" :extensions="extensions" v-model:content="content" output="json"
+      placeholder="Write something ..." @onSelectionUpdate="onSelectionUpdate" @onTransaction="onTransaction" />
   </div>
 </template>
 
@@ -75,11 +75,15 @@ const extensions = [
   TextPlaceholder,
 ];
 
-const content = ref(
-  {});
+const editor = ref();
+const content = ref({});
 
 const onSelectionUpdate = (options) => {
   console.log(options);
   console.log(options.editor.state.selection.node?.attrs);
+};
+
+const onTransaction = () => {
+  console.log(editor.value.editor.getJSON());
 };
 </script>
